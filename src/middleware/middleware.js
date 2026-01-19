b07a0e7b294f2e8ddec69703a3f7fe8c5fca694c
Dhruv Gupta <dhruv.gupta@antino.com>
2026-01-19 16:16:25 +0530

    added rate limiting and per user limit

--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
+const rateLimit = require('express-rate-limit');
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,   
+  max: 100,                   
+  message: {
+    status: "fail",
+    message: "Too many requests from this IP, please try again after 15 minutes"
+  },
+  standardHeaders: true,
+  legacyHeaders: false
+});
+
 
 const setupMiddleware = (app) => {
+  app.use('/api', limiter);
   app.use(express.json());
   app.use(morgan("dev"));
   app.use(cookieParser());
